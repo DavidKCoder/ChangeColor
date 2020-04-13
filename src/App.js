8d2d01ddae31f2204c4e3820a303755a40a06e34
DavidKizinger <karapetyand91@gmail.com>
2020-04-13 11:24:54 +0400

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Input from "./component/Input";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Input />
+    </div>);
+}
+
